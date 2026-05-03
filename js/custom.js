231da11cadb3abc307b1fa26b6c444e6a9b90d08
Joe Alongi <contact@joealongi.dev>
2026-05-03 02:30:05 -0400

    Initial commit (squashed for author)

---
+++ js/custom.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    /***************** Navbar-Collapse ******************/
+
+    $(window).scroll(function () {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    /***************** Page Scroll ******************/
+
+    $(function () {
+        $('a.page-scroll').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    /***************** Scroll Spy ******************/
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    /***************** Owl Carousel ******************/
+
+    $("#owl-hero").owlCarousel({
+
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+        transitionStyle: "fadeUp",
+        autoPlay: true,
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+
+    });
+
+
+    /***************** Full Width Slide ******************/
+
+    var slideHeight = $(window).height();
+
+    $('#owl-hero .item').css('height', slideHeight);
+
+    $(window).resize(function () {
+        $('#owl-hero .item').css('height', slideHeight);
+    });
+
+    /***************** Wow.js ******************/
+
+    new WOW().init();
+
+    /***************** Preloader ******************/
+
+    var preloader = $('.preloader');
+    $(window).load(function () {
+        preloader.remove();
+    });
+})
